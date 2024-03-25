34bfb648971d7177a7efafe2063e2092d7d5ffc7
WinterWood0118 <dltjdals125@gmail.com>
2024-03-25 14:18:24 +0900

    15번 수정

--- app/src/main/java/com/example/programmersalgorithm/Solution15.kt
+++ app/src/main/java/com/example/programmersalgorithm/Solution15.kt
@@ -1,16 +1,6 @@
 package com.example.programmersalgorithm
+
 // 나머지가 1이 되는 수 찾기
 class Solution15 {
-    fun solution(n: Int): Int {
-        var answer: Int = 0
-        if(n in 3 .. 1000000){
-            for(i in 1..n){
-                if(n%i == 1){
-                    answer = i
-                    break
-                }
-            }
-        }
-        return answer
-    }
+    fun solution(n: Int): Int = (1..n). first { n % it == 1 }
 }
