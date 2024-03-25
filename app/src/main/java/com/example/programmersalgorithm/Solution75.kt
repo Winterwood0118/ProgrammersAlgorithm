6402f5db79a659d6c7e4293dca3589c67a4363e8
WinterWood0118 <dltjdals125@gmail.com>
2024-03-25 09:30:58 +0900

    76, 77번 풀이

--- app/src/main/java/com/example/programmersalgorithm/Solution75.kt
+++ app/src/main/java/com/example/programmersalgorithm/Solution75.kt
@@ -1,13 +1,51 @@
 package com.example.programmersalgorithm
 
+import kotlin.concurrent.thread
+import kotlin.time.measureTime
+
 
 fun main(){
-    val testList = "-1 1 2 4 -5"
-    val test = Solution75()
+/*    val testList = "-1 1 2 4 -5 6 9 777 8 2"
+    //val test = Solution75()
 
-    println(test.solution(testList))
+    val runTime = measureTime {
+        testList.split(" ").map { it.toInt() }.run {
+            "${maxOf { it }} ${minOf { it }}"
+        }
+    }
+    println(runTime)
+
+    val letTime = measureTime {
+        testList.split(" ").map { it.toInt() }.let {
+            "${it.maxOf { it }} ${it.minOf { it }}"
+        }
+    }
+    println(letTime)
+    //println(test.solution(testList))*/
+/*    thread1.start()
+    thread2.start()*/
+    val c = '4'
+    println(c.toUpperCase())
 }
 
+val thread1 = thread(start = false){
+    val letTime = measureTime {
+        val testList = "-1 1 2 4 -5 6 9 777 8 2"
+        testList.split(" ").map { it.toInt() }.let {
+            "${it.maxOf { it }} ${it.minOf { it }}"
+        }
+    }
+    println("let: $letTime")
+}
+val thread2 = thread(start = false){
+    val runTime = measureTime {
+        val testList = "-1 1 2 4 -5 6 9 777 8 2"
+        testList.split(" ").map { it.toInt() }.run {
+            "${maxOf { it }} ${minOf { it }}"
+        }
+    }
+    println("run: $runTime")
+}
 class Solution75 {
     fun solution(s: String): String {
         var answer = ""
---
+++ app/src/main/java/com/example/programmersalgorithm/Solution76.kt
@@ -0,0 +1,10 @@
+package com.example.programmersalgorithm
+
+class Solution76 {
+    fun solution(s: String): String = s.split(" ").map {
+        it.map { it }.mapIndexed { i, c ->
+            if (i == 0) c.toUpperCase()
+            else c.toLowerCase()
+        }.joinToString("")
+    }.joinToString(" ")
+}
