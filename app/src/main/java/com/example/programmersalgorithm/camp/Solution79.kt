0408661c31f5fb6feab1bf2329118ab7f7c51bc9
WinterWood0118 <dltjdals125@gmail.com>
2024-03-27 09:46:32 +0900

    79번 풀이

---
+++ app/src/main/java/com/example/programmersalgorithm/camp/Solution79.kt
@@ -0,0 +1,30 @@
+package com.example.programmersalgorithm.camp
+
+import kotlin.math.sqrt
+
+// 카펫
+
+fun main() {
+    println(Solution79().solution2(10, 2).toList())
+}
+class Solution79 {
+    fun solution(brown: Int, yellow: Int): IntArray {
+        val area = brown + yellow
+        val measureList = mutableListOf<Pair<Int, Int>>()
+        for (i in 1..Math.sqrt(area.toDouble()).toInt()) {
+            if (area % i == 0) {
+                measureList.add(i to area / i)
+            }
+        }
+        return measureList.first { 2 * (it.first + it.second - 2) == brown }.run {
+            intArrayOf(second, first)
+        }
+    }
+
+    fun solution2(brown: Int, yellow: Int): IntArray {
+        val area = brown + yellow
+        return (1..Math.sqrt(area.toDouble()).toInt())
+            .first { area % it == 0 && 2 * (area / it + it - 2) == brown }
+            .let { intArrayOf(area / it, it) }
+    }
+}
