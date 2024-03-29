c12e50c039494e1ca9b03aba589a4b4d90bde463
WinterWood0118 <dltjdals125@gmail.com>
2024-03-29 09:57:38 +0900

    81번 풀이 솔루션 2는 다른사람풀이

---
+++ app/src/main/java/com/example/programmersalgorithm/camp/Solution81.kt
@@ -0,0 +1,35 @@
+package com.example.programmersalgorithm.camp
+
+// N개의 최소 곻배수
+
+
+
+class Solution81 {
+    fun solution(arr: IntArray): Int {
+        var lcm = 1
+
+        for (i in arr){
+            lcm = (i * lcm) / gcd(i, lcm)
+            println(lcm)
+        }
+
+        return lcm
+    }
+
+    tailrec fun gcd(a: Int, b: Int): Int {
+        return if (b == 0) a else gcd(b, a % b)
+    }
+
+
+
+    fun solution2(arr: IntArray): Int {
+        var answer = 1
+        while(true) {
+            var x = 0
+            for(a in arr) x += answer%a
+            if(x==0) return answer
+            answer++
+        }
+        return answer
+    }
+}
